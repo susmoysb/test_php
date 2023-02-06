2c2446fdf607acf14f7fe069ac6412a5e0910c61
susmoysb <susmoysb@gmail.com>
2023-02-06 15:47:51 +0600

    first

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+    // header("Content-Type: text/plain");
+
+    interface A{
+        function go();
+    }
+
+ 
+    echo "str\ning";
+
+    echo "<br>" ;
+
+    function test(string $a){
+        return 'this is just a test ' . $a;
+    }
+
+    echo test('hhjk') ;
+
+
+
+
+  
+?>
